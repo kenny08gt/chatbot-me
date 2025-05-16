4439a14c3fc9220b15ad4d5f1a4a5837fd99ae9a
Alan Hurtarte <alan.hurtarte@gmail.com>
2025-05-16 10:59:08 -0600

    Init commit and env

---
+++ app.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+from openai import OpenAI
+from pypdf import PdfReader
+import json
+import requests
+import gradio as gr
+import os
+
+load_dotenv(override=True)
+
+
+def push(text):
+    requests.post(
+        "https://api.pushover.net/1/messages.json",
+        data={
+            "token": os.getenv("PUSHOVER_TOKEN"),
+            "user": os.getenv("PUSHOVER_USER"),
+            "message": text,
+        }
+    )
+
+def record_user_details(email, name="Name not provided", notes="not provided"):
+    push(f"Recording {name} with email {email} and notes {notes}")
+    return {"recorded": "ok"}
+
+def record_unknown_question(question):
+    push(f"Recording {question}")
+    return {"recorded": "ok"}
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from chatbot-me!")
+
+
+if __name__ == "__main__":
+    main()
